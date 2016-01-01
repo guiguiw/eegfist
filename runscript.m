ea4359fc6f33022ecdc5adadee4f8966ab32a664
Markus Fritscher <markus.fritscher@fau.de>
2016-01-02 00:42:20 +0100

    added runscript

---
+++ runscript.m
@@ -0,0 +1,16 @@
+parfor i = 1:109
+    for j = [3,4,7,8,11,12]
+        filename = 'S';
+        if i < 10
+            filename = strcat(filename, '00');
+        else i < 100
+            filename = strcat(filename, '0');
+        end
+        filename = strcat(filename,num2str(i),'R');
+        if j < 10
+            filename = strcat(filename, '0');
+        end
+        filename = strcat(filename, num2str(j));
+        createcsvfromdata(filename);
+    end
+end
